f43d2efc9a3cb0cea2a6bfed2f067b9614d89f39
Saravanabalaji <dsbalaji2001@gmail.com>
2024-09-10 17:59:20 +0530

    path correction for jwt token

--- Middleware/validateToken.js
+++ Middleware/validateToken.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 
 
 // Creating a Authentication Token to Aceess (login)
-exports.authenticatetoken = (req,res,next)=>{
+const authenticatetoken = (req,res,next)=>{
     const token = req.header('Authorization')?.split(' ')[1]
     if(!token){
         return res.status(404).json({message: 'Access Denied'})
@@ -15,4 +15,6 @@ exports.authenticatetoken = (req,res,next)=>{
         res.status(500).json({message: 'Invalid Token'})
     }
 }
+
+module.exports = authenticatetoken;
 
