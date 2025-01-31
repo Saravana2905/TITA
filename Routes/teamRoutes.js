d7d9b15e3e3fa10b5d37bc4cc69b2e0c1bc05f71
Saravanabalaji <dsbalaji2001@gmail.com>
2025-01-31 15:52:48 +0530

    31/01/2025 1.3.5

--- Routes/teamRoutes.js
+++ Routes/teamRoutes.js
@@ -5,6 +5,7 @@ const { createTeam, getAllTeam, updateTeam } = require('../Controller/team_Contr
 // Route to create a team member
 router.post('/createTeam', createTeam);
 router.get('/getAllTeam', getAllTeam);
+router.get('/getTeam/:id', getAllTeam);
 router.put('/updateTeam/:id', updateTeam);
 
 module.exports = router;
