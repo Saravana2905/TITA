eb598e2114f7ae44cc3ffe5fbc29066da2b27bf3
Saravanabalaji <dsbalaji2001@gmail.com>
2025-01-31 14:01:19 +0530

    31/01/2025 1.3.1

--- Routes/teamRoutes.js
+++ Routes/teamRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createTeam, getAllTeam } = require('../Controller/team_Controller');
+const { createTeam, getAllTeam, updateTeam } = require('../Controller/team_Controller');
 
 // Route to create a team member
 router.post('/createTeam', createTeam);
